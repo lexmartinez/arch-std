1473fa6a121140d76cf7d3de919e77c95d8a83d3
Lex Martinez <lex0316@gmail.com>
2022-08-26 18:29:56 -0500

    updating content pages ids

--- pages/api/pages/[page].js
+++ pages/api/pages/[page].js
@@ -2,9 +2,9 @@ import notion from '~/lib/notion'
 
 const pages = {
   home: 'fbbdd2dbfa404cc7b7d1956c8d38ed5f',
-  us: 'fbbdd2dbfa404cc7b7d1956c8d38ed5f',
-  projects: 'fbbdd2dbfa404cc7b7d1956c8d38ed5f',
-  contact: 'fbbdd2dbfa404cc7b7d1956c8d38ed5f',
+  us: 'cbe1192689924d29bff7743231b3e707',
+  projects: '7239b6d9ba554236be5b6e37ad2c27fc',
+  contact: '0b29e855ffff4e32b3c661e6167fd7a6',
 }
 
 const mapBlocks = async (blocks) => {
