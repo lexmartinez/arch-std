6ac2af11da40974ab30251923f6f0c00d3bae60d
Lex Martinez <lex0316@gmail.com>
2022-08-27 07:11:26 -0500

    fixing padding on page content - navbar

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 import Footer from './Footer'
 import NavBar from './NavBar'
+
+export const Content = styled.div`
+  padding-top: 2.5rem;
+`
 
 const Page = (props) => {
   return (
     <>
       <NavBar />
-      {props.children}
+      <Content>{props.children}</Content>
       <Footer />
     </>
   )
