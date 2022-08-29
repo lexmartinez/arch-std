d5193f5aac35948f12a7e12c9af91817a59049da
Lex Martinez <lex0316@gmail.com>
2022-08-28 22:38:07 -0500

    contact page

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -33,8 +33,8 @@ export default function Map() {
       {...options}
     >
       <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {geoData?.lat && geoData?.lng && (
         <Circle
